80fcd3d18e6238ca600106ba8df942b02f6682bb
Dieguidev <diegogaraycullas@gmail.com>
2023-09-12 20:35:25 -0500

    method reduce

---
+++ 05-reduce.js
@@ -0,0 +1,18 @@
+const totals = [1, 2, 3, 4];
+
+//*********************************************** */
+//metodo sin reduce
+//*********************************************** */
+let sum = 0;
+for (let index = 0; index < totals.length; index++) {
+  const element = totals[index];
+  sum = sum + element;
+}
+console.log(sum);
+
+
+//*********************************************** */
+//metodo con reduce
+//*********************************************** */
+const rta = totals.reduce((sum, element) => sum + element, 0);
+console.log("rta", rta);
